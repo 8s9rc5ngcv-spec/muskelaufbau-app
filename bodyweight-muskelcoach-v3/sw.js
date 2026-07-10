5f89a39c05c16e139a0b5c16cda43a6de9584070
8s9rc5ngcv-spec <8s9rc5ngcv@privaterelay.appleid.com>
2026-07-10 09:56:25 +0200

    Add files via upload

---
+++ bodyweight-muskelcoach-v3/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'muskelcoach-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './muskelcoach-icon-180.png',
+  './muskelcoach-icon-192.png',
+  './muskelcoach-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
